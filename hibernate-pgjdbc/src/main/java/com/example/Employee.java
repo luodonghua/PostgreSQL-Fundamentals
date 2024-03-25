a7ff4bef87fe2b9aaadab7b943105ebeb7c91d2b
Luo Donghua <luodonghua@gmail.com>
2024-03-25 08:34:20 +0800

    Initial commit for JDBC Examples

---
+++ hibernate-pgjdbc/src/main/java/com/example/Employee.java
@@ -0,0 +1,55 @@
+package com.example;
+
+import java.io.Serializable;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name = "employee")
+public class Employee implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+   
+    @GenericGenerator(name = "generator", strategy = "increment")
+    @Id
+    @GeneratedValue(generator = "generator")
+    @Column(name = "id")
+    private Integer ID;
+   
+    @Column(name = "name", nullable = false)
+    private String Name;
+   
+    @Column(name = "salary", nullable = false)
+    private Integer Salary;
+
+    public Integer getID() {
+        return ID;
+    }
+
+    public void setId(Integer ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public Integer getSalary() {
+        return Salary;
+    }
+
+    public void setSalary(Integer Salary) {
+        this.Salary = Salary;
+    }
+    public String toString(){
+        return "ID: "+ID+" Name: "+Name+" Salary: "+Salary;
+    }
+}
---
+++ maven-jdbc/src/main/java/com/example/HikariCPConnectionPoolProps.java
@@ -0,0 +1,43 @@
+package com.example;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class HikariCPConnectionPoolProps {
+ 
+    private static HikariDataSource CreateHikariCPDataSource() {
+        HikariConfig config = new HikariConfig("/HikariCPConnectionPoolProps.properties");
+        return new HikariDataSource(config);
+    }
+    public static void main(String[] args) {
+
+        try {
+            Connection conn = CreateHikariCPDataSource().getConnection();
+            System.out.println("Connected to the PostgreSQL server successfully.");
+            
+            Statement stmt = conn.createStatement();
+            stmt.execute("CREATE TABLE emp (id INT primary key, name TEXT, salary smallint)");   
+            stmt.execute("INSERT INTO emp SELECT x,'Employee-'||x,((1+random())/2*10000)::smallint FROM generate_series(1,5) x");
+
+            ResultSet rs = stmt.executeQuery("SELECT id, name, salary FROM emp ORDER BY id");
+            while (rs.next()) {
+                System.out.format("| %-2d | %-8s | %6d |%n",
+                        rs.getInt("id"),
+                        rs.getString("name"),
+                        rs.getInt("salary"));
+            }
+            stmt.execute("DROP TABLE emp");
+            stmt.close();
+            conn.close();
+
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
