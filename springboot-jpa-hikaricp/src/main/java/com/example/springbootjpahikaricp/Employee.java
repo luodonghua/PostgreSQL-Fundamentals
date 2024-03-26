6bd5104866086989f79f11f4b3d0a0ec6d24cb73
Donghua Luo <luodonghua@gmail.com>
2024-03-26 22:19:44 +0800

    add SprintBoot+JPA+HikariCP Example

---
+++ springboot-jpa-hikaricp/src/main/java/com/example/springbootjpahikaricp/Employee.java
@@ -0,0 +1,56 @@
+package com.example.springbootjpahikaricp;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "employee")
+public class Employee {
+    
+    private static final long serialVersionUID = 1L;
+    @Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer ID;
+
+    @Column(name = "name", nullable = false)
+    private String Name;
+
+    @Column(name = "salary", nullable = false)
+    private Integer Salary;
+
+    public Employee() {
+	}
+
+    public Employee(String Name, Integer Salary) {
+        this.Name = Name;
+        this.Salary = Salary;
+    }
+
+    public Integer getID() {
+        return ID;
+    }
+
+    public void setId(Integer ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public Integer getSalary() {
+        return Salary;
+    }
+
+    public void setSalary(Integer Salary) {
+        this.Salary = Salary;
+    }
+    
+    public String toString(){
+        return "ID: "+ID+" Name: "+Name+" Salary: "+Salary;
+    }
+}
